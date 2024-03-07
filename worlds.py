b180faf73d69cc8228de4ca5f7f45aed31d68dfe
Vigrel <eller.vini@gmail.com>
2024-03-07 13:32:29 +0100

    feat(gymEnv): add random world as examples

---
+++ worlds.py
@@ -0,0 +1,22 @@
+AGED_AIRPORT = """ 
+    wwwwwwwwwwwwwwwwwwww
+    w             g    w
+    w      w   g       w
+    w               o ww
+    w  w    w   o o    w
+    w w  wow   w o     w
+    ww     o     w o   w
+    w     w   w    o   w
+    w     ww           w
+    w   g              w
+    ww    ww           w
+    w           ww   w w
+    w  ow              w
+    w           g    w w
+    w       o  o    w  w
+    w w      w   w w w w
+    w  ow  w           w
+    w            o    ow
+    w    a wo          w
+    wwwwwwwwwwwwwwwwwwww
+    """
